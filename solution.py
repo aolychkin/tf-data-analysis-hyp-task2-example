63f436919ab46e79926d4217d24d3788cdcf69d2
Алексей <55784938+aolychkin@users.noreply.github.com>
2023-04-13 16:21:08 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,9 +8,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    statistic, critical_values, pvalue = anderson_ksamp([x, y])
-    alpha = 0.05
-    if statistic > critical_values[2]:
-        return True
-    else:
-        return False
+    return anderson_ksamp([x, y]).pvalue <= 0.05 
